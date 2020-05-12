de041f2ddbcd685898b87c72c30fdc7e3dec2eff
michael5522 <michaelchang.kuo@gmail.com>
2020-05-12 04:19:43 +0000

    working

--- main.js
+++ main.js
@@ -4,5 +4,8 @@ var gradeTable = new GradeTable($table);
 var $header = $(".header");
 var pageHeader = new PageHeader($header);
 
-var app = new App(gradeTable, pageHeader);
+var $form = $("form");
+var gradeForm = new GradeForm($form);
+
+var app = new App(gradeTable, pageHeader, gradeForm);
 app.start();
